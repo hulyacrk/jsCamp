01332728b9421e5b90ce80c83bc201dea8510e02
Hulya Cirakogullari <hulyacirakogullari@gmail.com>
2021-10-19 15:17:41 +0300

    jscamp

--- src/app.js
+++ src/app.js
@@ -2,14 +2,25 @@ console.log("Merhaba Kodlama.io")
 
 //JS typesafe değildir...
 let dolarBugun = 9.30
+let dolarDun = 9.20
+console.log(dolarDun)
 
-//console.log(dolarDun)
+{
+    let dolarDun = 9.10
+    //let olduğunda console bunu görmez
+    //ama çalışan bir koddur
+    //var kullanılırsa hata verir.
+    //var global bir değişken gibi çalışır. 
+}
 
 const euroDun = 11.2
-//euroDun = 11 //bu hata verir
+//euroDun = 11 bu hata verir çünkü ilk atanan değer geçerlidir.
 
 console.log(euroDun)
 
+//const ile bir değişken tanımlandığında ona yeni bir değer atanamaz.
+//const sabittir.
+//değer - referans ile ilgilidir.
 
 
 let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi", "Özel Konut Kredisi"]
